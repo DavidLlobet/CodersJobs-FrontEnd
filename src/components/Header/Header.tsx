76857a8c93aa3468b0d46623634f553d8301ad75
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2022-03-14 23:35:34 +0100

    Solved some styling issues

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,17 @@
 import "./Header.css";
 
 const Header = (): JSX.Element => (
-        <nav className="header">
-            <ul className="header__logotitle">
-                <li className="header__logo list-item"> Logo </li>
+        <nav className="header navbar navbar-default">
+          <div className="container-fluid">
+            <ul className="header__logotitle navbar-left">
+                <li className="header__logo list-item">  </li>
                 <li className="header__title list-item"> <p> CodersJobs </p></li>
             </ul>
-            <ul className="header__list">
+            <ul className="header__list navbar-right">
                 <li className="list-item">Anadir Job</li>
                 <li className="list-item">Login</li>
             </ul>
+            </div>
         </nav>
     );
 
