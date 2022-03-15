385d5b8148e0c79bba1d6299a31b3ff0bafce5a5
David <david.llobet6@gmail.com>
2022-03-15 15:21:19 +0100

    Add conditional rendering in Header component for login button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,19 +4,29 @@ import "./Header.scss";
 const Header = (): JSX.Element => {
   const navigate = useNavigate();
 
+  const { pathname } = window.location;
+
+  const homeClick = () => {
+    navigate("/");
+  };
+
   const loginClick = () => {
     navigate("/login");
   };
 
   return (
     <div className="header">
-      <div className="header__title">
+      <div className="header__title" onClick={homeClick}>
         <p className="header__title1">Coders</p>
         <p className="header__title2">Jobs</p>
       </div>
-      <button className="header__login" onClick={loginClick}>
-        Entra
-      </button>
+      {pathname !== "/login" ? (
+        <button className="header__login" onClick={loginClick}>
+          Entra
+        </button>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
