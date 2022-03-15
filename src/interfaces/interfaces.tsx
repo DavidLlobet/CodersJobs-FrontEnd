600fcdcf7817972afd56562421a7e382606da48a
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2022-03-15 11:40:51 +0100

    Added actionTypes

---
+++ src/interfaces/interfaces.tsx
@@ -0,0 +1,26 @@
+export interface IJob {
+  title: string,
+  company: string,
+  companyAnchor: string,
+  jobAnchor: string,
+  description: string,
+  contactPerson: string,
+  salary: number,
+  numberOfWorkers?: number,
+  startup: boolean,
+  location: string,
+  desiredProfile: string,
+  image?: string, 
+  releaseDate: Date,
+}
+
+
+export interface IUser {
+  name?: string,
+  username: string,
+  password: string,
+  email?: string,
+  jobsApplied?: Array<Object>,
+  isAdmin: boolean,
+}
+
