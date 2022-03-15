684a734f882e6b223556640ec70266e9597124d1
David <david.llobet6@gmail.com>
2022-03-15 17:05:38 +0100

    Add question to register in LoginPage component

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -5,6 +5,10 @@ const LoginPage = (): JSX.Element => (
   <>
     <p className="title">Iniciar sesión</p>
     <LoginForm />
+    <div className="register">
+      <p className="register__text">¿No tienes cuenta? Regístrate</p>
+      <p className="register__click">aquí</p>
+    </div>
   </>
 );
 
