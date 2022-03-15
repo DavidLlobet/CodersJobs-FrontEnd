fd1b4ff20108e6a5a315cbf23bb5ef528981ce42
David <david.llobet6@gmail.com>
2022-03-15 15:46:56 +0100

    Create changerUserData function in LoginForm component

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 
-const LoginForm = (): JSX.Element => (
-  <form className="login-form" noValidate autoComplete="off">
-    <p className="login-form__username">Nombre de usuario</p>
-    <label htmlFor="username"></label>
-    <input
-      type="text"
-      id="username"
-      placeholder="Introduce tu nombre de usuario"
-      name="username"
-      className="login-form__input"
-      // value={userData.username}
-      required
-      // onChange={changeUserData}
-    ></input>
-  </form>
-);
+const LoginForm = (): JSX.Element => {
+  const initialData = {
+    username: "",
+    password: "",
+  };
+
+  const [userData, setUserData] = useState(initialData);
+
+  const changeUserData = (event: ChangeEvent<HTMLInputElement>) => {
+    setUserData({
+      ...userData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  return (
+    <form className="login-form" noValidate autoComplete="off">
+      <p className="login-form__text">Nombre de usuario</p>
+      <label htmlFor="username"></label>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        className="login-form__input"
+        value={userData.username}
+        required
+        onChange={changeUserData}
+      ></input>
+      <p className="login-form__text">Contraseña</p>
+      <label htmlFor="password"></label>
+      <input
+        type="text"
+        id="password"
+        name="password"
+        className="login-form__input"
+        value={userData.password}
+        required
+        onChange={changeUserData}
+      ></input>
+      <button type="submit" className="login-form__button">
+        Iniciar sesión
+      </button>
+    </form>
+  );
+};
 
 export default LoginForm;
