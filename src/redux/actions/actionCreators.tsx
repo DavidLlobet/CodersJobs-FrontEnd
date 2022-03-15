767709a2851769fd196ee669bf4d25ffa07da70d
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2022-03-15 15:40:00 +0100

    Added getJobsThunk

--- src/redux/actions/actionCreators.tsx
+++ src/redux/actions/actionCreators.tsx
@@ -1,10 +1,11 @@
 import actionTypes from "./actionTypes";
+import { IJob } from "../../interfaces/interfaces";
 
 export interface IJobs {
   jobs: Array<Object>;
 }
 
-export const getJobsAction = (jobs: IJobs) => ({
+export const getJobsAction = (jobs: IJob[]) => ({
   type: actionTypes.getJobs,
   payload: jobs,
 });
--- src/redux/store/store.tsx
+++ src/redux/store/store.tsx
@@ -1,9 +1,16 @@
-import { applyMiddleware, createStore } from "redux";
+import { AnyAction, applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+import thunk, {ThunkAction} from 'redux-thunk';
  
 import rootReducer from "../reducers/rootReducer";
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
+export type RootState = ReturnType<typeof rootReducer>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk = ThunkAction<void, RootState, unknown, AnyAction>;
+
 export default store;
+
