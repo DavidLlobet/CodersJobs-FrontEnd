61bb844a0c0b4e200d394ec34d9b0c0695d49dfa
David <david.llobet6@gmail.com>
2022-03-16 18:12:49 +0100

    Create test for App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
 import App from "./App";
+import store from "./redux/store/store";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/CodersJobs/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const header = screen.getByTitle("header");
+  const footer = screen.getByTitle("footer");
+
+  expect(header).toHaveClass("header");
+  expect(footer).toHaveClass("footer");
 });
